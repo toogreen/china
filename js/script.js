09bc19964c7bf5534fbf9bcdefacb72b7930a482
David Gagnon <toogreen@15inch.lan>
2020-07-16 16:46:50 -0400

    improved the structure of the JSON data and added 2 items from page title and slogan. Also modified the script to reflect those changes so that instead of having to specify a number for each title, desc, etc, we refer to news1, news2, news3 instead and so on. Also now it counts the number of items from the JSON object automatically so that we don't have to manualy input that number anymore, we can simply add as many news items as we wish and it will adapt automatically

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@ xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
 
+    document.getElementById("title").innerHTML = myObj.title;
+    document.getElementById("slogan").innerHTML = myObj.slogan;
+
     // added this loop so that I don't have to repeat the same code many times to fetch the data (ex. Title1, Title2, etc)
     var i;
-    var count = 6; // Change this value according to how many articles are in the news.txt file
+    var count = Object.keys(myObj).length - 1; // This counts how many items are in the JSON object, and removes 1 since we have 2 extra items (page title+slogan) but need 1 extra since loop count starts at 1
+
     for (i = 1; i < count; i++) {
+        var news = "news" + i; 
         var img = "img" + i;
         var title = "title" + i;
         var desc = "desc" + i;
         var url = "url" + i;
-        document.getElementById(img).src = myObj.news[img];
-        document.getElementById(title).innerHTML = myObj.news[title];
-        document.getElementById(desc).innerHTML = myObj.news[desc];
-        document.getElementById(url).href = myObj.news[url];
+        document.getElementById(img).src = myObj[news].img;
+        document.getElementById(title).innerHTML = myObj[news].title;
+        document.getElementById(desc).innerHTML = myObj[news].desc;
+        document.getElementById(url).href = myObj[news].url;
     } // End of loop
+
+    //document.getElementById("slogan").innerHTML = count;
 
   }
 };
